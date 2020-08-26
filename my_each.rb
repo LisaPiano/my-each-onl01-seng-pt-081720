1222bcc996eedd9bceb6773bfe6ef5f963c93bc5
Lisa Walters <campi_l@fortlewis.edu>
2020-08-26 18:16:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,17 @@
 def my_each(array)
+  if block_given?
   counter = 0
   while counter < array.length
     yield(array[i])
     counter += 1
+  end
+  array
+else
+  puts "Hey! No block was given!"
 
 end
 end
-collection = [1, 2, 3, 4]
+
 my_each(collection)
 do |i|
 puts i
